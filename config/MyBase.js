771774f566873e6f1187715d5b6b815c74351d28
Jinwoo Kawk <tkfkdnwjd@gmail.com>
2020-10-26 17:34:01 +0900

    test auth

---
+++ config/MyBase.js
@@ -0,0 +1,26 @@
+import firebase from "firebase";
+import {
+    API_KEY,
+    AUTH_DOMAIN,
+    DATABASE_URL,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_ID,
+    APP_ID,
+    MEASUREMENT_ID,
+} from "@env";
+
+var firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    databaseURL: DATABASE_URL,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID,
+};
+// Initialize Firebase
+const myBase = firebase.initializeApp(firebaseConfig);
+
+export default myBase;
